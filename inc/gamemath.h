f6cb8d046cd86495062eedddf3f7334979f255b8
Danilo Bezerra <danilobezerra@outlook.com.br>
2024-01-25 00:53:10 -0300

    Set const-qualifications only in function definitions

--- inc/gamemath.h
+++ inc/gamemath.h
@@ -3,12 +3,12 @@
 
 #include <genesis.h>
 
-V2f16 v2_add(const V2f16 *const a, const V2f16 *const b);
-V2f16 v2_sub(const V2f16 *const a, const V2f16 *const b);
-f16 v2_cross(const V2f16 *const a, const V2f16 *const b);
-f16 v2_dot(const V2f16 *const a, const V2f16 *const b);
-V2f16 v2_scale(const V2f16 *const a, const f16 s);
-V2f16 v2_lerp(const V2f16 *const a, const V2f16 *const b, const f16 t);
+V2f16 v2_add(const V2f16 *a, const V2f16 *b);
+V2f16 v2_sub(const V2f16 *a, const V2f16 *b);
+f16 v2_cross(const V2f16 *a, const V2f16 *b);
+f16 v2_dot(const V2f16 *a, const V2f16 *b);
+V2f16 v2_scale(const V2f16 *a, f16 s);
+V2f16 v2_lerp(const V2f16 *a, const V2f16 *b, f16 t);
 
 extern const f16 f16s_0;
 extern const f16 f16s_1;
--- inc/intersection.h
+++ inc/intersection.h
@@ -4,13 +4,13 @@
 #include <genesis.h>
 
 bool Intersections_is_segs_intersections(
-    const V2f16 *const o1, const V2f16 *const e1,
-    const V2f16 *const o2, const V2f16 *const e2
+    const V2f16 *o1, const V2f16 *e1,
+    const V2f16 *o2, const V2f16 *e2
 );
 
 bool Intersections_is_box_intersecting_seg(
-    const V2f16 *const box_origin, const V2f16 *const box_size,
-    const V2f16 *const seg_origin, const V2f16 *const seg_ending
+    const V2f16 *box_origin, const V2f16 *box_size,
+    const V2f16 *seg_origin, const V2f16 *seg_ending
 );
 
 #endif // MAKE_ME_LAUGH_INTERSECTION_H

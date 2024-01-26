fb367ad16fed9fd446db694d6b292c3dbe0ba4b6
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2024-01-25 23:31:16 -0300

    rope implemented

--- src/rope.c
+++ src/rope.c
@@ -1,11 +1,16 @@
 #include "rope.h"
+#include "gamemath.h"
 #include "global.h"
+#include "maths.h"
 
 void Rope_draw(const V2f16 *const p1, const V2f16 *const p2) {
-    const fix16 len = (fix16) ROPE_LENGTH;
-    const V2s16 size = { .x = 2, .y = 2 };
+    const f16 len = FIX16(ROPE_LENGTH);
+    const f16 lenm1 = len - 1; // m1: minus 1
 
-    for (fix16 i = 0; i < ROPE_LENGTH; ++i) {
-        // TODO: Render Draw Rect
+    PAL_setColor(2, RGB24_TO_VDPCOLOR(0xFFFFFF));
+
+    for (f16 i = 0; i < len; ++i) {
+        const V2f16 p = v2_lerp(p1, p2, fix16Div(i, lenm1));
+        VDP_fillTileMapRect(BG_A, TILE_ATTR_FULL(PAL0, 0, FALSE, FALSE, 2), p.x, p.y, 2, 2);
     }
 }

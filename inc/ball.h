158e3667ff149f125e1c2cbfaa13c8578831b17e
Danilo Bezerra <danilobezerra@outlook.com.br>
2024-01-26 00:06:31 -0300

    Set const-qualifications only in function definitions

--- inc/ball.h
+++ inc/ball.h
@@ -8,8 +8,8 @@ typedef struct {
     V2f16 velocity;
 } Ball;
 
-Ball Ball_init(const V2f16 pos);
-void Ball_apply_gravity(Ball *ball, fix16 gravity_acc);
+Ball Ball_init(V2f16 pos);
+void Ball_apply_gravity(Ball *ball, f16 gravity_acc);
 void Ball_update(Ball *ball, const V2f16 *p1, const V2f16 *p2);
 void Ball_draw(const Ball *ball);
 
--- inc/entity.h
+++ inc/entity.h
@@ -3,8 +3,8 @@
 
 #include <genesis.h>
 
-void Entity_translate(V2f16 *const position, const V2f16 *const translation);
-Box Entity_bounding_box(const V2f16 *position, const V2u16 size);
-void Entity_draw(const Box *const bb, const u16 color);
+void Entity_translate(V2f16 *position, const V2f16 *translation);
+Box Entity_bounding_box(const V2f16 *position, V2u16 size);
+void Entity_draw(const Box *bb, u16 color);
 
 #endif //MAKE_ME_LAUGH_ENTITY_H

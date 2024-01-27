6ea996732eaa552e59713c5f57bd7af8472709f8
Danilo Bezerra <danilobezerra@outlook.com.br>
2024-01-27 00:42:08 -0300

    Fix player order

--- src/player.c
+++ src/player.c
@@ -10,7 +10,7 @@ Player Player_init(u8 player_id, V2f16 pos) {
 }
 
 void Player_update(Player *const player) {
-    u16 player_input = JOY_readJoypad(player->entity_id ? JOY_1 : JOY_2);
+    u16 player_input = JOY_readJoypad(player->entity_id == 0 ? JOY_1 : JOY_2);
 
     if (player_input != 0) {
         V2f16 movement = { .x = f16s_0, .y = f16s_0 };
@@ -41,5 +41,5 @@ V2f16 Player_get_center(const Player *const player) {
 
 void Player_draw(const Player *const player) {
     const Box player_bb = Entity_bounding_box(&player->position, (V2u16){.x = 1, .y = 1 });
-    Entity_draw(&player_bb, player->entity_id ? RGB24_TO_VDPCOLOR(0xFF0000) : RGB24_TO_VDPCOLOR(0x0000FF));
+    Entity_draw(&player_bb, player->entity_id == 0 ? RGB24_TO_VDPCOLOR(0xFF0000) : RGB24_TO_VDPCOLOR(0x0000FF));
 }

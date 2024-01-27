a6071b5309b1eb5ce32a39b79ac714c324b347dd
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2024-01-27 02:30:13 -0300

    fix merge

--- src/game.c
+++ src/game.c
@@ -13,8 +13,6 @@ V2f16 p0_center;
 V2f16 p1_center;
 
 void Game_init(Game *game) {
-    VDP_init();
-
     JOY_init();
     SPR_init();
 
@@ -35,6 +33,7 @@ void Game_run(Game *game) {
         Game_update(game);
         Game_draw(game);
 
+        SPR_update();
         SYS_doVBlankProcess();
     }
 }
@@ -68,14 +67,15 @@ void Game_update(Game *game) {
 
 void Game_draw(Game *game) {
     kprintf(":: GAME DRAW ::");
+
+    VDP_clearPlane(BG_A, TRUE);
+    DMA_waitCompletion();
+
+    VDP_clearPlane(BG_B, TRUE);
+    DMA_waitCompletion();
+
     switch (game->current_state) {
         case STATE_SPLASH:
-            VDP_clearPlane(BG_A, TRUE);
-            DMA_waitCompletion();
-
-            VDP_clearPlane(BG_B, TRUE);
-            DMA_waitCompletion();
-
             VDP_drawImageEx(BG_B, &splash, TILE_ATTR_FULL(PAL0, FALSE, FALSE, FALSE, user_index), 0, 0, FALSE, TRUE);
             user_index += splash.tileset->numTile;
 
@@ -90,9 +90,6 @@ void Game_draw(Game *game) {
         case STATE_MAIN_MENU:
             break;
         case STATE_GAMEPLAY:
-            VDP_clearPlane(BG_A, TRUE);
-            VDP_clearPlane(BG_B, TRUE);
-
             for (int i = 0; i < PLAYER_COUNT; i++) {
                 kprintf("-- PLAYER %d--", i);
                 Player_draw(&game->players[i]);
@@ -105,6 +102,4 @@ void Game_draw(Game *game) {
 
             break;
     }
-
-    SPR_update();
 }

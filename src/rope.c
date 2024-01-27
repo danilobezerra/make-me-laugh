16f7d9fdbb5f043efb44058ca2b656df5d1d3f4e
André Luiz Alvares <andrluiz.alvares.alvares84@gmail.com>
2024-01-27 00:16:56 -0300

    readd rope

--- src/rope.c
+++ src/rope.c
@@ -10,11 +10,11 @@ void Rope_draw(const V2f16 *const p1, const V2f16 *const p2) {
     PAL_setColor(2, RGB24_TO_VDPCOLOR(0xFFFFFF));
 
     for (int i = 0; i < ROPE_LENGTH; ++i) {
-        // V2f16 p = v2_lerp(p1, p2, fix16Div(intToFix16(i), lenm1));
-        // p.x = fix16Div(p.x, f16s_16);
-        // p.y = fix16Div(p.y, f16s_16);
-        // s16 px = fix16ToRoundedInt(p.x);
-        // s16 py = fix16ToRoundedInt(p.y);
-        // VDP_fillTileMapRect(BG_A, TILE_ATTR_FULL(PAL0, 0, FALSE, FALSE, 2), px, py, 1, 1);
+        V2f16 p = v2_lerp(p1, p2, fix16Div(intToFix16(i), lenm1));
+        p.x = fix16Div(p.x, f16s_16);
+        p.y = fix16Div(p.y, f16s_16);
+        s16 px = fix16ToRoundedInt(p.x);
+        s16 py = fix16ToRoundedInt(p.y);
+        VDP_fillTileMapRect(BG_A, TILE_ATTR_FULL(PAL0, 0, FALSE, FALSE, 2), px, py, 1, 1);
     }
 }
